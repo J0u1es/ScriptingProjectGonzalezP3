2d4787a10fe8f9f27e6b105e0b0fc3c165900d4d
Julia Gonzalez <145380988+J0u1es@users.noreply.github.com>
2023-11-02 10:37:28 -0700

    Finished If Statements assignments
    Finished the scripting assignment where we made a coffee timer.

---
+++ ScriptingProjectP3Gonzalez/Assets/Scripts/CoffeeTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeTimer : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemerature = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+void TemperatureTest ()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+
+        else if(coffeeTemperature < coldLimitTemerature) 
+        {
+            print("Coffee is too cold.");
+        }
+        else
+        {
+            print("Coffee is just right.");
+        }
+
+    }
+
+
+
+}
