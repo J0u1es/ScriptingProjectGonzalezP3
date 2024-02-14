4cbfc25bfaeb3f66d9e136dd4b34fc4a98bb0ea3
Julia Gonzalez <145380988+J0u1es@users.noreply.github.com>
2024-02-14 10:42:02 -0800

    Finished the Destroy Scripting Assignment
    I did it. :)

--- ScriptingProjectP3Gonzalez/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectP3Gonzalez/Assets/Scripts/LinearInterpolation.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class LinearInterpolation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
 
-    // Update is called once per frame
-    void Update()
-    /*{
-        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
-    }*/
+    // Here result = (4, 5, 6)
+ 
+    // Start is called before the first frame update
+    /* void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+         light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+     }*/
 }
